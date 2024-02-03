faa7f8ce00dc2603e38711cc789182569c424268
dante <dante276@yandex.ru>
2024-02-03 12:46:54 +0300

    add UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,14 +1,13 @@
 package jm.task.core.jdbc.dao;
 
-import com.mysql.cj.Session;
 import jm.task.core.jdbc.model.User;
-import org.hibernate.SessionFactory;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.security.auth.login.Configuration;
+import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
-
 
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -16,34 +15,128 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
-    @Override
+@Override
     public void createUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Query query = session.createSQLQuery("create table if NOT EXISTS users\n" +
+                    "(\n" +
+                    "    id       bigint auto_increment\n" +
+                    "        primary key,\n" +
+                    "    age      tinyint      null,\n" +
+                    "    lastName varchar(255) null,\n" +
+                    "    name     varchar(255) null\n" +
+                    ")\n" +
+                    "    engine = MyISAM;\n");
+            query.executeUpdate();
+            transaction.commit();
+            session.close();
+        } catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+        }
 
     }
-
     @Override
     public void dropUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Query query = session.createSQLQuery("DROP Table IF EXISTS users");
+            query.executeUpdate();
+            transaction.commit();
+            session.close();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
 
     }
+
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+
+            Query query = session.createSQLQuery("INSERT INTO users (name, lastName, age) "
+                    + "VALUES(?, ?, ?)");
+            query.setParameter(1, name);
+            query.setParameter(2, lastName);
+            query.setParameter(3, age);
+            query.executeUpdate();
+            //session.persist(query);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Query query = session.createSQLQuery("DELETE FROM users WHERE ID = ?");
+            query.setParameter(1, id);
+            query.executeUpdate();
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getSessionFactory().openSession();
+        List<User> list = new ArrayList<>();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.get(User.class, 1L);
+            Query query = session.createQuery("from User");
+            list  = query.getResultList();
+            session.close();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+
+        return list;
     }
+
 
     @Override
     public void cleanUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction  transaction = session.beginTransaction();
+        try {
+            Query query = session.createSQLQuery("DELETE FROM users");
+            query.executeUpdate();
+            transaction.commit();
+            session.close();
 
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,6 +15,7 @@ public class Util {
     private static final String DB_USERNAME = "Utkativ";
     private static final String DB_PASSWORD = "root";
 
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnectionBD() {
         Connection connection = null;
@@ -23,6 +28,26 @@ public class Util {
             System.out.println("Connection ERROR");
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory == null){
+            synchronized (Util.class){
+                if(sessionFactory == null){
+                    return sessionFactory = new Configuration()
+                            .setProperty("hibernate.connection.url","jdbc:mysql://localhost:3306/pp.1.1.4")
+                            .setProperty("hibernate.connection.username","Utkativ")
+                            .setProperty("hibernate.connection.password","root")
+                            .setProperty("show_sql, true","create")
+                            .setProperty("hibernate.current_session_context_class","thread")
+                            .setProperty("hibernate.current_session_context_class","thread")
+                            .setProperty("hibernate.connection.autocommit","true")
+                            .addAnnotatedClass(User.class).
+                            buildSessionFactory();
+                }
+            }
+        }
+        return sessionFactory;
     }
 
 }
